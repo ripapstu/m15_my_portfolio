1562499c0e1dbbf45e6fb9750da2f86e51ec5ac7
ripapstu <ripapstu@gmail.com>
2025-03-15 20:54:38 +0600

    project upload

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Pages Routes
+Route::get('/', [SiteController::class, 'homePage'])->name('home');
+Route::get('/about', [SiteController::class, 'aboutPage'])->name('about');
+Route::get('/services', [SiteController::class, 'servicesPage'])->name('services');
+Route::get('/portfolio', [SiteController::class, 'portfolioPage'])->name('portfolio');
+Route::get('/contact', [SiteController::class, 'contactPage'])->name('contact');
